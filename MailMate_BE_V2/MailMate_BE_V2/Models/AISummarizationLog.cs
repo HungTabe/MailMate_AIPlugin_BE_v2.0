281943fad115713aa42ecb2a0c31816dd6f2b43c
hungtabe <trandinhhung717@gmail.com>
2025-05-09 21:58:17 +0700

    [mm_net_18] Update DB - API Summarize and HuggingFace

---
+++ MailMate_BE_V2/MailMate_BE_V2/Models/AISummarizationLog.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MailMate_BE_V2.Models
+{
+    public class AISummarizationLog
+    {
+        [Key]
+        public Guid LogId { get; set; }
+
+        [Required]
+        [StringLength(450)]
+        public string EmailId { get; set; }
+
+        [StringLength(100)]
+        public string ModelName { get; set; }
+
+        public int InputLength { get; set; }
+
+        public int OutputLength { get; set; }
+
+        public int ExecutionTimeMs { get; set; }
+
+        [StringLength(50)]
+        public string Status { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [ForeignKey("EmailId")]
+        public virtual Email Email { get; set; }
+    }
+}
