f2827f2c13426712dbc3b06d58c53f3572d4215b
hungtabe <trandinhhung717@gmail.com>
2025-05-09 22:33:43 +0700

    [mm_net_18B] API Get Profile of User

--- MailMate_BE_V2/MailMate_BE_V2/Controllers/AuthController.cs
+++ MailMate_BE_V2/MailMate_BE_V2/Controllers/AuthController.cs
@@ -1,7 +1,10 @@
 ﻿using MailMate_BE_V2.DTO;
 using MailMate_BE_V2.DTOs;
 using MailMate_BE_V2.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace MailMate_BE_V2.Controllers
 {
@@ -50,6 +53,28 @@ namespace MailMate_BE_V2.Controllers
             catch (Exception ex)
             {
                 return BadRequest(new { Message = ex.Message });
+            }
+        }
+
+        [HttpGet("profile")]
+        [Authorize]
+        public async Task<ActionResult<UserProfileDto>> GetProfile()
+        {
+            try
+            {
+                var userId = User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value
+                    ?? User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (string.IsNullOrEmpty(userId))
+                {
+                    return Unauthorized(new { message = "Invalid user" });
+                }
+
+                var profile = await _authService.GetUserProfileAsync(userId);
+                return Ok(profile);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred while fetching profile", details = ex.Message });
             }
         }
     }
